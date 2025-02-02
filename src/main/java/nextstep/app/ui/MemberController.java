5ff324eba33115fbca542ef98d857e22444f3b50
zinzo <zinzoddari@gmail.com>
2025-02-03 01:07:21 +0900

    BasicAuthenticationFilter 분리

--- src/main/java/nextstep/app/ui/MemberController.java
+++ src/main/java/nextstep/app/ui/MemberController.java
@@ -1,11 +1,11 @@
 package nextstep.app.ui;
 
+import jakarta.servlet.http.HttpServletRequest;
 import nextstep.app.domain.Member;
 import nextstep.app.domain.MemberRepository;
-import nextstep.app.util.Base64Convertor;
+import nextstep.security.domain.Authentication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,16 +20,11 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public ResponseEntity<List<Member>> list(@RequestHeader("Authorization") String authorization) {
+    public ResponseEntity<List<Member>> list(HttpServletRequest request) {
         try {
-            String credentials = authorization.split(" ")[1];
-            String decodedString = Base64Convertor.decode(credentials);
-            String[] usernameAndPassword = decodedString.split(":");
-            String username = usernameAndPassword[0];
-            String password = usernameAndPassword[1];
-
-            memberRepository.findByEmail(username)
-                    .filter(it -> it.matchPassword(password))
+            final Authentication userDetails = (Authentication) request.getAttribute("Authentication");
+            memberRepository.findByEmail(userDetails.getUsername())
+                    .filter(it -> it.matchPassword(userDetails.getPassword()))
                     .orElseThrow(AuthenticationException::new);
 
             List<Member> members = memberRepository.findAll();
